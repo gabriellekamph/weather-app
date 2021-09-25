c34cc9f5271eab635180b1e0ccfafdeea87b6482
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-09-25 23:05:04 +0200

    Correcting server.js file for deploy

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-// Server file
+//server.js
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express()
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
 
-// All urls goto to index.html in /dist folder [build folder]
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.use("/", serveStatic(path.join(__dirname, '/dist')))
-app.get(/.*/, function (req, res) {
-res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-const port = process.env.PORT || 8081;
-app.listen(port)
-console.log(`App is listening on port: ${port}`)
+app.listen(port);
